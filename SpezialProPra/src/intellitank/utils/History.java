b763070edd286468157352e928f764faadf33b2e
Martixx <Martixx@192.168.2.102>
2017-11-20 16:26:54 +0100

    1

--- SpezialProPra/src/intellitank/utils/History.java
+++ SpezialProPra/src/intellitank/utils/History.java
@@ -1,15 +1,34 @@
 package intellitank.utils;
 
-import java.util.Date;
+import java.util.HashMap;
+
+import intellitank.main.Timestamp;
 
 public class History
 {
-	public static String getPrice(int id, Date date)
+
+	public static String getPrice(int id, Timestamp timestamp)
 	{
+
+		String[] bTime = Reader.readURL("https://raw.githubusercontent.com/InformatiCup/InformatiCup2018/master/Eingabedaten/Benzinpreise/" + id +".csv").split("|");
+		for (int i = 0; i < bTime.length; i++) {
+
+
+		}		
 		String result = "";
-		
+
 		// TODO
+		HashMap<Timestamp, Integer> data = new HashMap<>();
+
+		for (int i = 0; i < bTime.length; i++)
+		{
+			data.put(Timestamp.fromString(bTime[i].split(";")[0]), Integer.valueOf((bTime[i].split(";")[0])));
+		}	
 		
+		if(data.containsKey(timestamp))
+				result = String.valueOf(data.get(timestamp));
+		
+			
 		return result;
 	}
 }
