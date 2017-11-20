7b629a6e4d2f1284476688bad479c4dd8fcdc810
Boy132 <if-mathe@gmx.de>
2017-11-20 16:51:33 +0100

    added Calculator (TODO)

---
+++ SpezialProPra/src/intellitank/utils/Calculator.java
@@ -0,0 +1,18 @@
+package intellitank.utils;
+
+public class Calculator
+{
+	// 1.	YYYY-MM-DD HH:MM:SS+HH bis zu dem Sie die Benzinpreise als bekannt annehmen und für Ihre	Berechnungen verwenden dürfen
+	// 2.	geforderten	Zeitpunkt für die Vorhersage
+	// 2.	Tankstellen-ID für die Vorhersage
+	
+	// 2015-02-10 12:18:01+01	;	2015-02-15 21:18:01+01	;	24
+	public static String getPriceAt(String data)
+	{
+		String result = "";
+		
+		// TODO
+		
+		return result;
+	}
+}
