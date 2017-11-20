ba8b7d5df12156282fad8f5b22ac98a7bbf59910
Martixx <Martixx@192.168.2.102>
2017-11-20 16:41:01 +0100

    getPrice in History Class added

--- SpezialProPra/src/intellitank/utils/History.java
+++ SpezialProPra/src/intellitank/utils/History.java
@@ -9,24 +9,18 @@ public class History
 
 	public static String getPrice(int id, Timestamp timestamp)
 	{
-
-		String[] bTime = Reader.readURL("https://raw.githubusercontent.com/InformatiCup/InformatiCup2018/master/Eingabedaten/Benzinpreise/" + id +".csv").split("|");
-		for (int i = 0; i < bTime.length; i++) {
-
-
-		}		
 		String result = "";
 
-		// TODO
+		String[] bTime = Reader.readURL("https://raw.githubusercontent.com/InformatiCup/InformatiCup2018/master/Eingabedaten/Benzinpreise/" + id +".csv").split("|");
+		
 		HashMap<Timestamp, Integer> data = new HashMap<>();
-
 		for (int i = 0; i < bTime.length; i++)
 		{
 			data.put(Timestamp.fromString(bTime[i].split(";")[0]), Integer.valueOf((bTime[i].split(";")[0])));
 		}	
 		
 		if(data.containsKey(timestamp))
-				result = String.valueOf(data.get(timestamp));
+			result = String.valueOf(data.get(timestamp));
 		
 			
 		return result;
