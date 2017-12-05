a19a98acfed29ebdad8b14754f1f3b5a5852d88a
Boy132 <if-mathe@gmx.de>
2017-12-05 16:49:54 +0100

    added TimeSeries methods (TODO)

--- SpezialProPra/src/intellitank/main/TimeSeries.java
+++ SpezialProPra/src/intellitank/main/TimeSeries.java
@@ -1,11 +1,34 @@
 package intellitank.main;
 
+import java.util.HashMap;
+
+import intellitank.utils.Timestamp;
+
 public class TimeSeries
 {
-	
+	private HashMap<Timestamp, Integer> values = new HashMap<>();
 	
 	public TimeSeries()
 	{
 		
+	}
+	
+	public boolean addValue(Timestamp time, Integer value)
+	{
+		if(!values.containsKey(time))
+		{
+			values.put(time, value);
+			return true;
+		}
+		
+		return false;
+	}
+	
+	public Integer getValue(Timestamp time)
+	{
+		if(values.containsKey(time))
+		{
+			return values.get(time);
+		} else return 0;
 	}
 }
