f6735f9edd85b641231f67e7e500c249fd6883b5
Boy132 <if-mathe@gmx.de>
2018-01-08 13:18:07 +0100

    added StationList frame

---
+++ SpezialProPra/src/intellitank/utils/StationList.java
@@ -0,0 +1,18 @@
+package intellitank.utils;
+
+import java.util.ArrayList;
+
+public class StationList
+{
+	private ArrayList<Gasstation> stations;
+	
+	public StationList()
+	{
+		this.stations = new ArrayList<>();
+	}
+	
+	public static void read()
+	{
+		// TODO //
+	}
+}
